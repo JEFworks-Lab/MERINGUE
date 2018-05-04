39ce0f5126d31b2e3a552572009181b118b44c81
jefworks <jeanfan@fas.harvard.edu>
2018-05-04 14:28:39 -0400

    nearest neighbor calculations

--- R/helper.R
+++ R/helper.R
@@ -21,17 +21,72 @@ getAdj <- function(mat, k) {
   return(adj)
 }
 
+#' Mutual nearest neighbors
+getMnn <- function(ctA, ctB, pos, k) {
+    # ctB's nearest ctA neighbors
+    knnB <- RANN::nn2(pos[ctA,], pos[ctB,], k=k)[[1]]
+    knnB.named <- matrix(ctA[knnB], nrow=nrow(knnB), ncol=ncol(knnB))
+    rownames(knnB.named) <- ctB
+    # ctA's nearest ctB neighbors
+    knnA <- RANN::nn2(pos[ctB,], pos[ctA,], k=k)[[1]]
+    knnA.named <- matrix(ctB[knnA], nrow=nrow(knnA), ncol=ncol(knnA))
+    rownames(knnA.named) <- ctA
+
+    # mutual nearest neighbors
+    ## mnnB <- lapply(1:nrow(knnB.named), function(i) {
+    ##     vi <- sapply(1:k, function(j) {
+    ##         rownames(knnB.named)[i] %in% knnA.named[knnB.named[i,j],]
+    ##     })
+    ##     knnB.named[i,][vi]
+    ## })
+    ## names(mnnB) <- rownames(knnB.named)
+    mnnA <- lapply(1:nrow(knnA.named), function(i) {
+        vi <- sapply(1:k, function(j) {
+            rownames(knnA.named)[i] %in% knnB.named[knnA.named[i,j],]
+        })
+        knnA.named[i,][vi]
+    })
+    names(mnnA) <- rownames(knnA.named)
+
+    # get adjacency matrix
+    mnn <- mnnA
+    adj <- matrix(0, length(c(ctA, ctB)), length(c(ctA, ctB)))
+    rownames(adj) <- colnames(adj) <- c(ctA, ctB)
+    invisible(lapply(seq_len(length(mnn)), function(i) {
+        # symmetric (if I'm your mutual nearest neighbor, you're mine)
+        adj[names(mnn)[i],
+            mnn[[i]]
+            ] <<- 1
+        adj[mnn[[i]],
+            names(mnn)[i]
+            ] <<- 1
+    }))
+    return(adj)
+}
+
+#' Nearest background neighbor
+getBnn <- function(cct, nct, pos, k) {
+    knn <- RANN::nn2(pos[nct,], pos[cct,], k=k)[[1]]
+    adj <- matrix(0, nrow(pos), nrow(pos))
+    rownames(adj) <- colnames(adj) <- rownames(pos)
+    invisible(lapply(seq_len(length(cct)), function(i) {
+        adj[cct[i],nct[knn[i,]]] <<- 1
+    }))
+    return(adj)
+}
+
+
 #' Plot neighbor network
 #' https://stackoverflow.com/questions/43879347/plotting-a-adjacency-matrix-using-pure-r
 plotNetwork <- function(pos, adj, col='black', line.col='red', line.power=1, ...) {
-  plot(pos, pch=16, col=col)
+  plot(pos, pch=16, col=col, ...)
   idx <- which(adj>0, arr.ind = T)
   for(i in seq_len(nrow(idx))) {
     lines(
       c(pos[idx[i,1],1], pos[idx[i,2],1]),
       c(pos[idx[i,1],2], pos[idx[i,2],2]),
       col=line.col,
-      lwd=adj[idx]^line.power
+      lwd=adj[idx]^line.power,
     )
   }
 }
